203a61fdcf4c6577d504a1bdd84c8319b033a3b0
Expectation Failed <14265536+Code417@users.noreply.github.com>
2017-12-01 13:53:55 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,9 @@
 #usage:
-#		 wget -nv -O - https://raw.githubusercontent.com/Code417/Home/master/installer.sh 2>/dev/null | VERBOSE=true bash
-#############################################################################
-
-#this script assumes (cp, local, type ) as shell built-in
-#this script assumes ( [[, [, awk, cat) from BusyBox
-#this script assumes (wget, whoami) from BusyBox
-#{} prevents exicution until file is fully downloaded
+#         wget -nv -O - https://raw.githubusercontent.com/Code417/Home/master/installer.sh 2>/dev/null | VERBOSE=true bash
+# this script assumes (cp, local, type ) as shell built-in
+# this script assumes ( [[, [, awk, cat) from BusyBox
+# this script assumes (wget, whoami) from BusyBox
+# {} prevents exicution until file is fully downloaded
 {
 SRC="https://github.com/Code417/Home/archive/master.zip"
 mkdir -pv ~/tmp/home
