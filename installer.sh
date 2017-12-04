20a8638b68df92d0f14d13e9fbb507ac67f75fcc
Expectation Failed <14265536+Code417@users.noreply.github.com>
2017-12-04 12:28:10 -0500

    Implemented curl, and added new if
    Implemented some necessary changes in order to cope with wgets inability to download over HTTPS on some systems. Primarily the replacement of wget with curl, as well as the inclusion of a secondary if statement to deal with download failure.

--- installer.sh
+++ installer.sh
@@ -3,15 +3,19 @@
 # this script assumes (cp, local, type ) as shell built-in
 # this script assumes ( [[, [, awk, cat) from BusyBox
 # this script assumes (wget, whoami) from BusyBox
+# this script assumes (curl) from System
+
 # {} prevents exicution until file is fully downloaded
 {
 SRC="https://github.com/Code417/Home/archive/master.zip"
 mkdir -pv ~/tmp/home
 if [ ! -f ~/tmp/master.zip ]; then
-	wget -O ~/tmp/master.zip $SRC
+	curl $SRC --output ~/tmp/master.zip --silent
 fi
 SRC=~/tmp/home/Home-master
-unzip -o ~/tmp/master.zip -d ~/tmp/home
+if [ -f ~/tmp/master.zip ]; then
+	unzip -o ~/tmp/master.zip -d ~/tmp/home
+fi
 cp -r --no-clobber $SRC/.* ~/ 2>/dev/null
 #rm -r ~/tmp/home
 }
